567c54bad539265ee58a5b55016c55d9103db12f
caleb <kalejaiyecaleb@gmail.com>
2024-07-15 00:33:59 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import NavBar from "./layouts/NavBar";
 
 function App() {
   return (
-    
+    <div className="bg-green-700">
+      <NavBar />
+    </div>
   )
 }
 
---
+++ src/layouts/NavBar.jsx
@@ -0,0 +1,27 @@
+
+import { logo } from "../assets";
+import { Search } from "lucide-react";
+
+const NavBar = () => {
+    return (
+        <nav className="flex justify-between bg-green-700 items-center">
+            <img className="w-[150px]" src={logo} alt="Logo" />
+            <div className="flex justify-center items-center">
+            <div className="flex justify-center items-center">
+                <Search />
+                <input type="search" placeholder="Want to learn?" className="h-[30px] w-[150px] border-none outline-none px-4 rounded-[5px]" />
+            </div>
+            <ul className="ml-14 flex justify-center items-center">
+                <li><a href="">HI</a></li>
+            </ul>
+            </div>
+
+            <div className="flex justify-center items-center">
+                <a href=""></a>
+                <a href=""></a>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
